677d7857b33c17f0fa575d9c91d80e11e4a9ce4c
Рустам <rustam19871203@gmail.com>
2022-12-27 22:23:13 +0400

    Добавьте файлы проекта.

---
+++ Functions_2/Functions_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int luckeNumber(int number) {
+    if (number / 2) {
+        cout << "Число " << number << " является счастливым!" << endl;
+    }
+    else {
+        cout << "Число " << number << " не является счастливым.";
+    }
+    return number;
+}
+int main()
+{
+    int number = 0;
+    cout << "Введите любое число и проверим является ли оно счастливым: ";
+    cin >> number;
+    number = luckeNumber(number);
+}
